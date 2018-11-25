c17775888538ebbec8542c50a09b169cb045a327
carsoncao <funnycodedog@126.com>
2018-11-25 23:40:03 +0800

    fix bug

--- src/main/java/com/caolch/kmbridge/Utils/ScreenUtil.java
+++ src/main/java/com/caolch/kmbridge/Utils/ScreenUtil.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class ScreenUtil {
 
-    public List<Resolution> getResolutions() {
-        List<Resolution> rls = new ArrayList<Resolution>();
-        GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice[] devices = g.getScreenDevices();
-
-        for (int i = 0; i < devices.length; i++) {
-            System.out.println("Width:" + devices[i].getDisplayMode().getWidth());
-            System.out.println("Height:" + devices[i].getDisplayMode().getHeight());
-        }
-    }
+//    public List<Resolution> getResolutions() {
+//        List<Resolution> rls = new ArrayList<Resolution>();
+//        GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
+//        GraphicsDevice[] devices = g.getScreenDevices();
+//
+//        for (int i = 0; i < devices.length; i++) {
+//            System.out.println("Width:" + devices[i].getDisplayMode().getWidth());
+//            System.out.println("Height:" + devices[i].getDisplayMode().getHeight());
+//        }
+//    }
 
 }
--- src/main/java/com/caolch/kmbridge/server/Executor.java
+++ src/main/java/com/caolch/kmbridge/server/Executor.java
@@ -2,6 +2,7 @@ package com.caolch.kmbridge.server;
 
 import javax.swing.*;
 import java.awt.event.KeyAdapter;
+import java.lang.reflect.InvocationTargetException;
 
 public class Executor {
     public static void main(String[] args) {
